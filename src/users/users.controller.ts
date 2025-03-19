2b5cb729f46ac389c19a41d338c334d059807a12
Oleksandr Diachenko <dyachenko664@gmail.com>
2025-03-19 16:50:50 +0200

    add user actions

---
+++ src/users/users.controller.ts
@@ -0,0 +1,81 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+  Session,
+  UseGuards
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { User } from './user.entity';
+import { UserDto } from './dtos/user.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { AuthGuard } from '../guards/auth.guard';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user;
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+
+    return user;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+
+    return user;
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string): Promise<User | null> {
+    const user = await this.usersService.findOne(Number(id));
+    if (!user) throw new NotFoundException('user not found');
+
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string): Promise<User[]> {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string): Promise<User> {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string): Promise<User> {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+
+  findOne(id: number): Promise<User | null> | null {
+    if (!id) return null;
+
+    return this.repo.findOne({ where: { id } });
+  }
+
+  find(email: string): Promise<User[]> {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>): Promise<User> {
+    const user = await this.findOne(id);
+
+    if (!user) throw new Error('user not found');
+
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number): Promise<User> {
+    const user = await this.findOne(id);
+
+    if (!user) throw new Error('user not found');
+
+    return this.repo.remove(user);
+  }
+}
